feb2095b6f55eb0b7c819ef66e2d376d32777430
Hadiyo11 <hadyhope@gmail.com>
2023-10-30 12:12:50 -0700

    added mongo

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
-const path = require('path')
+// const path = require('path')
 
 // Connect DB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    //useCreateIndex: true,
-    useUnifiedTopology: true,
-    //useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB is connected"))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     //useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     //useFindAndModify: false,
+//   })
+//   .then(() => console.log("MongoDB is connected"))
+//   .catch((err) => console.log(err));
 
 app.use(cors());
 // Body parser
@@ -23,19 +23,31 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 
-// Serve static assets (build folder) if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-// get anything, load index.html file
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-}
+
+// // Serve static assets (build folder) if in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
+// // get anything, load index.html file
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-// prod false bcz  it will not run build script if its in prod, once its done it will be in prod
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// // prod false bcz  it will not run build script if its in prod, once its done it will be in prod
 
-// login heroku , create new app
+// // login heroku , create new app
